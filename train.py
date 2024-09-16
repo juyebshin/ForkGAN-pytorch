ce4c0dba11bc5e07459ac5a73112c1dc5cb43f31
Wonjune Kim <culigan3186@gmail.com>
2024-09-16 19:07:17 +0900

    Fix bug

--- train.py
+++ train.py
@@ -76,7 +76,7 @@ if __name__ == '__main__':
                 model.compute_visuals()
                 visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
                 save_image_grid(model.get_current_visuals(), os.path.join(test_dir, f'{epoch}_{epoch_iter}.jpg'))
-                if opt.instance_level != 'none':
+                if opt.instance_level:
                     save_image_grid_inst(model.get_current_visuals(), os.path.join(test_dir, f'{epoch}_{epoch_iter}_inst.jpg'))
 
             if total_iters % opt.print_freq == 0 and opt.gpu == 0:    # print training losses and save logging information to the disk
